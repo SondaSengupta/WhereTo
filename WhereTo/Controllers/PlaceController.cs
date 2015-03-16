205082f2bfb74ffd02e889888ba0ee28d4e40a34
SondaSengupta <sondasengupta@gmail.com>
2015-03-16 11:36:08 -0500

    Get placecontroller

---
+++ WhereTo/Controllers/PlaceController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WhereTo.Models;
+using WhereTo.Repository;
+
+namespace WhereTo.Controllers
+{
+    public class PlaceController : ApiController
+    {
+        private static PlaceRepository _db = new PlaceRepository();
+
+        // GET: api/PlaceController
+        public IQueryable<Place> GetPlaces()
+        {
+            return _db.GetAllPlaces();
+        }
+
+        //Post api/PlaceController
+        public HttpResponseMessage Post(HttpRequestMessage request, Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(place);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            return request.CreateResponse(HttpStatusCode.BadRequest, GetErrorMessages());
+        }
+
+        private object GetErrorMessages()
+        {
+            return ModelState.Values.SelectMany(x => x.Errors.Select(e => e.ErrorMessage));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
