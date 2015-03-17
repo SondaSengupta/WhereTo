6409a07ea7cd5441f880cfdf4423d9093caac290
SondaSengupta <sondasengupta@gmail.com>
2015-03-17 09:31:43 -0500

    made angular views and routing working

--- WhereTo/Scripts/App/place-controller.js
+++ WhereTo/Scripts/App/place-controller.js
@@ -1,3 +1,13 @@
-﻿angular.module('WhereToApp', [])
+﻿/// <reference path="Views/ViewDestination.html" />
+/// <reference path="Views/AddDestination.html" />
+angular.module('WhereToApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/ViewDestination", {
+            templateUrl: "/templates/ViewDestination.html", controller: "PlaceCtrl"
+        });
+        $routeProvider.when("/AddDestination", {
+            templateUrl: "/templates/AddDestination.html", controller: "PlaceCtrl"
+        });
+    })
 .controller('PlaceCtrl', function ($scope, $http) {
 })
