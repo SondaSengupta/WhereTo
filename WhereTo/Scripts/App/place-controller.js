fda61ed588650445ed7c58850db6aba20ed1b5af
SondaSengupta <sondasengupta@gmail.com>
2015-03-22 14:32:53 -0500

    seperated

--- WhereTo/Scripts/App/place-controller.js
+++ WhereTo/Scripts/App/place-controller.js
@@ -7,14 +7,6 @@
             templateUrl: "/templates/AddDestination.html", controller: "PlaceCtrl"
         });
     })
-    //.config(function (uiGmapGoogleMapApiProvider) {
-    //    console.log("Onward to Google!");
-    //    uiGmapGoogleMapApiProvider.configure({
-    //        key: 'AIzaSyDzuf3PKADN4hVI4ry8q0bVMsgFMPq_ofk',
-    //        v: '3.17',
-    //        libraries: 'weather, places'
-    //    });
-    //})
 .controller('PlaceCtrl', function ($scope, placeRepository, $location) {
     $scope.places = placeRepository.get();
 
@@ -22,7 +14,59 @@
         placeRepository.save(place);
         $location.url("/ViewDestination");
     }
+    
+    $scope.mapstart = function initialize() {
+        var mapOptions = {
+            center: { lat: -33.8688, lng: 151.2195 },
+            zoom: 13
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
 
+        var input = (document.getElementById('pac-input'));
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            infowindow.close();
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                return;
+            }
+
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);
+            }
+
+            // Set the position of the marker using the place ID and location
+            marker.setPlace({
+                placeId: place.place_id,
+                location: place.geometry.location
+            });
+            marker.setVisible(true);
+
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                'Place ID: ' + place.place_id + '<br>' +
+                place.formatted_address);
+            infowindow.open(map, marker);
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', $scope.mapstart);
     
     
 })
