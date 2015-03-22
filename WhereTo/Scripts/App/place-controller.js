e74f38749a24ef846449dd036540fee28204d649
SondaSengupta <sondasengupta@gmail.com>
2015-03-22 04:32:30 -0500

    initial setup for google maps

--- WhereTo/Scripts/App/place-controller.js
+++ WhereTo/Scripts/App/place-controller.js
@@ -1,4 +1,4 @@
-﻿angular.module('WhereToApp', ['ngRoute', 'ngResource'])
+﻿angular.module('WhereToApp', ['ngRoute', 'ngResource', 'uiGmapgoogle-maps'])
     .config(function ($routeProvider) {
         $routeProvider.when("/ViewDestination", {
             templateUrl: "/templates/ViewDestination.html", controller: "PlaceCtrl"
@@ -7,6 +7,14 @@
             templateUrl: "/templates/AddDestination.html", controller: "PlaceCtrl"
         });
     })
+    //.config(function (uiGmapGoogleMapApiProvider) {
+    //    console.log("Onward to Google!");
+    //    uiGmapGoogleMapApiProvider.configure({
+    //        key: 'AIzaSyDzuf3PKADN4hVI4ry8q0bVMsgFMPq_ofk',
+    //        v: '3.17',
+    //        libraries: 'weather, places'
+    //    });
+    //})
 .controller('PlaceCtrl', function ($scope, placeRepository, $location) {
     $scope.places = placeRepository.get();
 
@@ -14,4 +22,45 @@
         placeRepository.save(place);
         $location.url("/ViewDestination");
     }
+
+        var mapOptions = {
+            zoom: 4,
+            center: new google.maps.LatLng(40.0000, -98.0000),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        $scope.markers = [];
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        var createMarker = function (info) {
+
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(info.lat, info.long),
+                title: info.city
+            });
+            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+
+            $scope.markers.push(marker);
+
+        }
+
+        for (i = 0; i < cities.length; i++) {
+            createMarker(cities[i]);
+        }
+
+        $scope.openInfoWindow = function (e, selectedMarker) {
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        }
+    
+    
 })
